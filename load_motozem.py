8014be12f6a3d446e7070d52386a79d0f610f715
Amik1990 <djarnot@seznam.cz>
2024-05-28 20:18:37 +0200

    Added function test_banner_motozem....

---
+++ load_motozem.py
@@ -0,0 +1,19 @@
+import pytest
+from playwright.sync_api import Page, sync_playwright, expect
+
+
+def test_load_motozem(page):
+    page.goto("https://www.motozem.cz/")
+    accept_cookies = page.get_by_role("link", name="OK", exact=True)
+    accept_cookies.click()
+
+
+def test_banner_motozem_is_visible(page):
+    page.goto("https://www.motozem.cz/")
+    accept_cookies = page.get_by_role("link", name="OK", exact=True)
+    accept_cookies.click()
+
+    banner_motozem = page.get_by_role("banner").get_by_role("link", name="MotoZem", exact=True)
+    expect(banner_motozem).to_be_visible()
+
+
