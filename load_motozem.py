de65f5a01f939dc31df891d98c18f607884294a8
Amik1990 <djarnot@seznam.cz>
2024-05-29 13:21:14 +0200

    Přidán test_informace_menu_content

--- load_motozem.py
+++ load_motozem.py
@@ -14,3 +14,33 @@ def test_banner_motozem_is_visible(page, load_motozem):
     expect(banner_motozem).to_be_visible()
 
 
+def test_informace_menu_is_visible_when_hover_over(page, load_motozem):
+    informace_button = page.get_by_role("link", name="Informace")
+    informace_menu = page.get_by_text("O nás Obchodní podmínky Ochrana osobních údajů Velkoobchod Prodejny Vnitřní")
+    informace_button.hover()
+    expect(informace_menu).to_be_visible()
+
+
+def test_informace_menu_content(page, load_motozem):
+    informace_button = page.get_by_role("link", name="Informace")
+    informace_button.hover()
+
+    informace_items = [
+        {"selector": page.get_by_title("O nás"), "text": "O nás"},
+        {"selector": page.get_by_title("Obchodní podmínky"), "text": "Obchodní podmínky"},
+        {"selector": page.get_by_title("Ochrana osobních údajů"), "text": "Ochrana osobních údajů"},
+        {"selector": page.get_by_title("Velkoobchod"), "text": "Velkoobchod"},
+        {"selector": page.get_by_title("Prodejny"), "text": "Prodejny"},
+        {"selector": page.get_by_title("Vnitřní oznamovací systém"), "text": "Vnitřní oznamovací systém"},
+        {"selector": page.get_by_title("Výměna, vrácení, reklamace"), "text": "Výměna, vrácení, reklamace"},
+        {"selector": page.get_by_role("link", name="Kariéra"), "text": "Kariéra"},
+    ]
+
+    for item in informace_items:
+        menu_item = item["selector"]
+        expect(menu_item).to_be_visible()
+        expect(menu_item).to_have_text(item["text"])
+
+
+
+
