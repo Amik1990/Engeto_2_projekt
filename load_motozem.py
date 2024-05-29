98999e4383b0943e713698be45e5517edf31613d
Amik1990 <djarnot@seznam.cz>
2024-05-29 09:46:11 +0200

    Přidán import funkce load_mototozem ze souboru conftest

--- load_motozem.py
+++ load_motozem.py
@@ -1,15 +1,15 @@
 import pytest
 from playwright.sync_api import Page, sync_playwright, expect
+from conftest import load_motozem
 
 
-@pytest.fixture()
 def test_load_motozem(page):
     page.goto("https://www.motozem.cz/")
     accept_cookies = page.get_by_role("link", name="OK", exact=True)
     accept_cookies.click()
 
 
-def test_banner_motozem_is_visible(page, test_load_motozem):
+def test_banner_motozem_is_visible(page, load_motozem):
     banner_motozem = page.get_by_role("banner").get_by_role("link", name="MotoZem", exact=True)
     expect(banner_motozem).to_be_visible()
 
